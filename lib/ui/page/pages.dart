7673a533335a0d624f60e90063026cb8fd1ba934
salahulbain <salahul.bain@gmail.com>
2022-09-17 21:01:00 +0700

    general page ui

---
+++ lib/ui/page/pages.dart
@@ -0,0 +1,8 @@
+import 'package:absen_usm_v2/shared/shared.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:supercharged/supercharged.dart';
+
+part 'general_page.dart';
